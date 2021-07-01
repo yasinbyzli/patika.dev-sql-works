b142bfec46a7f00d8e63e39a995bb93c0fbde0f0
yasinbyzli <yasinbeeyazli@gmail.com>
2021-07-01 21:58:02 +0300

    completed work 3

---
+++ 03-work/post.sql
@@ -0,0 +1,30 @@
+/* 1- country tablosunda bulunan country sütunundaki ülke isimlerinden 
+'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız. */
+SELECT * FROM country
+WHERE
+country LIKE 'A%a'
+
+/* 2- country tablosunda bulunan country sütunundaki ülke isimlerinden 
+en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız. */
+SELECT * FROM country
+WHERE
+char_length(country) >= 6 
+AND 
+country LIKE '%n'
+
+/* 3- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük 
+harf farketmesizin 'T' karakteri içeren */
+SELECT * FROM film
+WHERE
+char_length(title) >= 4 
+AND 
+title ~~* '%T%'
+/* 4- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan 
+ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız. */
+SELECT * FROM film
+WHERE
+title LIKE 'C%'
+AND 
+length > 90
+AND
+rental_rate IN (2.99)
