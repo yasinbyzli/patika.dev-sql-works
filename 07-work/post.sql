11431a81425553fc1b8438602419321fc9ea9102
yasinbyzli <yasinbeeyazli@gmail.com>
2021-07-31 18:24:14 +0300

    completed work 7

---
+++ 07-work/post.sql
@@ -0,0 +1,20 @@
+/* 1-  film tablosunda bulunan filmleri rating değerlerine göre gruplayınız. */
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating
+
+/* 2-  film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan 
+    replacement_cost değerini ve karşılık gelen film sayısını sıralayınız. */
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+Having COUNT(*) > 50
+
+/* 3- customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? */
+SELECT store_id, COUNT(*) FROM customer 
+GROUP BY store_id
+
+/* 4- city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla 
+    şehir sayısı barındıra country_id bilgisini ve şehir sayısını paylaşınız. */
+SELECT country_id, COUNT(*) from city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC 
+LIMIT 1
